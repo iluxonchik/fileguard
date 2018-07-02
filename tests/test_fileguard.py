b4ef52872aeeba9a3ad5b1a5c81f2cc1c1da6001
ILUXONCHIK <illya@iluxonchik.me>
2018-07-02 21:45:05 +0100

    test: add class decorator tests

--- tests/test_fileguard.py
+++ tests/test_fileguard.py
@@ -136,3 +136,82 @@ class TestFileGuardContextManager(unittest.TestCase):
             self._assert_file_content_equals(lines_to_write)
 
         self._assert_file_content_equals(TestFileGuardContextManager.TEST_FILE_CONTENTS)
+
+
+class TestFileGuardClassDecorator(unittest.TestCase):
+
+    TEST_TEXT_FILE_PATH = './tests/resources/test_text_file.txt'
+    TEST_FILE_CONTENTS = ['would\n', 'you do it\n', 'if my name was\n', 'dre\n']
+
+    @guard('./tests/resources/test_text_file.txt')
+    class TheCodeumentary(object):
+
+        TEXT_1 = ['The\n', 'Documentary\n']
+        TEXT_2 = ['The\n', 'Doctor\'s\n', 'Advocate']
+
+        def __init__(_self, value_1, value_2, test_case):
+            _self._value_1 = value_1
+            _self._value_2 = value_2
+            _self._test_case = test_case  # allow asserts in methods
+
+        @property
+        def value_1(self):
+            return self._value_1
+
+        def change_the_file(_self):
+            lines_to_write = TestFileGuardClassDecorator.TheCodeumentary.TEXT_1
+
+            with open(TestFileGuardClassDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
+                file.writelines(lines_to_write)
+
+            _self._test_case._assert_file_content_equals(TestFileGuardClassDecorator.TheCodeumentary.TEXT_1)
+
+        def change_the_file_again(_self):
+            lines_to_write = TestFileGuardClassDecorator.TheCodeumentary.TEXT_2
+
+            with open(TestFileGuardClassDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
+                file.writelines(lines_to_write)
+
+            _self._test_case._assert_file_content_equals(TestFileGuardClassDecorator.TheCodeumentary.TEXT_2)
+
+        def do_not_change_the_file(_self, value):
+            _self._value_3 = 'L.A.X.'
+            _self._value_3 = _self._value_3 + value
+
+    def setUp(self):
+        with open(TestFileGuardClassDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
+            file.writelines(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+    def tearDown(self):
+        try:
+            os.remove(TestFileGuardClassDecorator.TEST_TEXT_FILE_PATH)
+        except FileNotFoundError:
+            pass
+
+    def _assert_file_content_equals(self, lines):
+        with open(TestFileGuardClassDecorator.TEST_TEXT_FILE_PATH, 'r') as file:
+            file_contents = file.readlines()
+
+        self.assertEqual(len(lines), len(file_contents))
+
+        for i in range(len(lines)):
+            self.assertEqual(lines[i], file_contents[i], f'File differs in line {i}')
+
+    def test_decorated_class_restores_file_contents(self):
+        the_codeumentary = TestFileGuardClassDecorator.TheCodeumentary('value_1', 'value_2', self)
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+        the_codeumentary.change_the_file()
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+        the_codeumentary.change_the_file_again()
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+        the_codeumentary.do_not_change_the_file('hello')
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+        the_codeumentary.value_1
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)
+
+        the_codeumentary._value_2 = 'value_3'
+        self._assert_file_content_equals(TestFileGuardClassDecorator.TEST_FILE_CONTENTS)

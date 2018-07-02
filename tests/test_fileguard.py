cc3d7b5d18a83ac43a4b71edfdf567af3c943d4b
ILUXONCHIK <illya@iluxonchik.me>
2018-07-02 14:24:57 +0100

    test: add test to ensure that a removed file is restored

--- tests/test_fileguard.py
+++ tests/test_fileguard.py
@@ -1,69 +1,91 @@
 import unittest
 import os
+from pathlib import Path
 from unittest.mock import Mock
 from fileguard.fileguard import guard
 
-class TestRestoreFileContentsDecorator(unittest.TestCase):
+class TestFileGuardDecorator(unittest.TestCase):
 
     TEST_TEXT_FILE_PATH = './tests/resources/test_text_file.txt'
     TEST_FILE_CONTENTS = ['would\n', 'you do it\n', 'if my name was\n', 'dre\n']
 
     def setUp(self):
-        with open(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
-            file.writelines(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+        with open(TestFileGuardDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
+            file.writelines(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
     def tearDown(self):
         try:
-            os.remove(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH)
+            os.remove(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
         except FileNotFoundError:
             pass
 
     def _assert_file_content_equals(self, lines):
-        with open(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH) as file:
+        with open(TestFileGuardDecorator.TEST_TEXT_FILE_PATH, 'r') as file:
             file_contents = file.readlines()
-            self.assertCountEqual(file_contents, lines)
 
-    def test_guard(self):
+        self.assertEqual(len(lines), len(file_contents))
 
-        @guard(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH)
+        for i in range(len(lines)):
+            self.assertEqual(lines[i], file_contents[i], f'File differs in line {i}')
+
+    def test_guard_change_text_file(self):
+        @guard(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
         def function_that_changes_the_file():
             lines_to_write = ['of course\n', 'I would\n']
-            self._assert_file_content_equals(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+            self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
-            with open(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH, 'r') as file:
+            with open(TestFileGuardDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
                 file.writelines(lines_to_write)
 
             self._assert_file_content_equals(lines_to_write)
 
         function_that_changes_the_file()
-        self._assert_file_content_equals(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+        self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
 
-    def test_guard_if_function_throws_exception(self):
-
-            @guard(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH)
+    def test_guard_if_function_throws_exception_text_file(self):
+            @guard(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
             def function_that_changes_the_file():
                 lines_to_write = ['of course\n', 'I would\n']
-                self._assert_file_content_equals(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+                self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
-                with open(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH, 'r') as file:
+                with open(TestFileGuardDecorator.TEST_TEXT_FILE_PATH, 'w') as file:
                     file.writelines(lines_to_write)
 
                 self._assert_file_content_equals(lines_to_write)
                 raise Exception('Something happened #Windows10')
 
-            function_that_changes_the_file()
-            self._assert_file_content_equals(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+            with self.assertRaises(Exception):
+                function_that_changes_the_file()
+            self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
 
-    def test_guard_if_function_not_changes_file(self):
+    def test_guard_if_function_not_changes_text_file(self):
 
-                @guard(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH)
+                @guard(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
                 def function_that_changes_the_file():
                     pass
 
                 function_that_changes_the_file()
-                self._assert_file_content_equals(TestRestoreFileContentsDecorator.TEST_FILE_CONTENTS)
+                self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
+
+    def test_guard_deleted_file_restores_contents_test_file(self):
+            @guard(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
+            def function_that_deletes_the_file():
+                os.remove(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
+
+                # make sure that the file does not exist after running the function
+                is_file = path.is_file()
+                self.assertFalse(is_file, 'File does exist.')
+
+            # make sure that the file is existent
+            path = Path(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
+            is_file = path.is_file()
+            self.assertTrue(is_file, 'File does not exist.')
+
+            function_that_deletes_the_file()
+
+            self._assert_file_content_equals(TestFileGuardDecorator.TEST_FILE_CONTENTS)
 
     def test_that_decorator_calls_funcion(self):
         value_1 = 'uno'
@@ -71,7 +93,7 @@ class TestRestoreFileContentsDecorator(unittest.TestCase):
 
         mocked_func = Mock()
 
-        pre_decorated = guard(TestRestoreFileContentsDecorator.TEST_TEXT_FILE_PATH)
+        pre_decorated = guard(TestFileGuardDecorator.TEST_TEXT_FILE_PATH)
         decorated = pre_decorated(mocked_func)
         result = decorated(value_1, value_2)
 

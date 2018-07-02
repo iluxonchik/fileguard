779deadc5676562cc0224f261b154cc3a10abfcf
ILUXONCHIK <illya@iluxonchik.me>
2018-07-02 12:03:17 +0100

    refactor file content saving and restoring
    File content saving and restoring functionalities are now in separate functions.
    This will allow code reuse for the various decorators.

--- fileguard/fileguard.py
+++ fileguard/fileguard.py
@@ -1,39 +1,51 @@
 from functools import wraps
 
+
 class _guard(object):
 
     def __init__(self, path):
         self._path = path
-
-    def decorate_callable(self, func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            path = self._path
-
-            orignal_file_contents = []
-            with open(path, 'r') as file:
-                for line in file:
-                    orignal_file_contents.append(line)
-
-            try:
-                func(*args, **kwargs)
-            except Exception:
-                pass
-
-            with open(path, 'w') as file:
-                for line in orignal_file_contents:
-                    file.write(line)
-
-        return wrapper
-
-    def decorate_class(self):
-        raise NotImplemented('Class wrapper not yet implemented.')
+        self._original = {}
 
     def __call__(self, func):
         if isinstance(func, type):
             return self.decorate_class(func)
         else:
             return self.decorate_callable(func)
+
+    def _store_original_content(self, path):
+        orignal_file_contents = []
+        with open(path, 'r') as file:
+            for line in file:
+                orignal_file_contents.append(line)
+        self._original[path] = orignal_file_contents
+
+    def _restore_original_content(self, path):
+        orignal_file_contents = self._original[path]
+
+    def _restore_original_file_contents(self, path, orignal_file_contents):
+        with open(path, 'w') as file:
+            for line in orignal_file_contents:
+                file.write(line)
+
+    def decorate_callable(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            path = self._path
+
+            self._store_original_content(path)
+
+            try:
+                func(*args, **kwargs)
+            except Exception:
+                pass
+
+            self._restore_original_content(path)
+
+        return wrapper
+
+    def decorate_class(self):
+        raise NotImplemented('Class wrapper not yet implemented.')
 
 def guard(path):
     """Preserve the contents of a file.
